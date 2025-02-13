3e017cdb2c3ba73f89ca86fb09585a595fd3daac
VeraGerber <84279244+VeraGerber@users.noreply.github.com>
2025-02-13 20:17:14 +0300

    Update models.py

--- phones/models.py
+++ phones/models.py
@@ -1,18 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    class Phone(models.Model):
-        # TODO: Добавьте требуемые поля
-        name = models.CharField(max_length=100)
-        price = models.IntegerField(default=0)
-        image = models.URLField(default='')
-        release_date = models.DateField(default=1)
-        lte_exists = models.BooleanField(default=False)
-        slug = models.SlugField(unique=True)
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    image = models.CharField(max_length=255)
+    release_date = models.DateTimeField(default=timezone.now)
+    lte_exists = models.BooleanField(default=False)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True,
+                            verbose_name="URL", default='', null=False)
 
-        def save(self, *args, **kwargs):
-            self.slug = slugify(self.name)
-            super(Phone, self).save(*args, **kwargs)
-    pass
+    def __str__(self):
+        return f'model - {self.name}, price - {self.price}, lte -  {self.lte_exists}'
