ca2beb886524954dc15a8f3569ca31b87fdbe833
VeraGerber <84279244+VeraGerber@users.noreply.github.com>
2025-02-13 20:17:47 +0300

    Update views.py

--- phones/views.py
+++ phones/views.py
@@ -1,4 +1,11 @@
+import csv
+from pprint import pprint
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
+
+from main.settings import CSV_PATH
+from phones.models import Phone
 
 
 def index(request):
@@ -7,12 +14,28 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {"phones": Phone.objects.all().order_by}
-    ({'name': 'name', 'min_price': 'price', 'max_price': '-price', None: 'id'}[request.GET.get('sort')])
+    content = Phone.objects.all()
+    sort = request.GET.get('sort', '')
+    if sort == 'name':
+        content = list(Phone.objects.all().order_by('name').values())
+    elif sort == 'min_price':
+        content = list(Phone.objects.all().order_by('price').values())
+    elif sort == 'max_price':
+        content = list(Phone.objects.all().order_by('-price').values())
+    page_number = int(request.GET.get("page", 1))
+    # paginator = Paginator(list(content), 10)
+    paginator = Paginator(content, 10)
+    page = paginator.get_page(page_number)
+    context = {
+        'phones': page,
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
     template = 'product.html'
-    context = {{"phone": Phone.objects.get(slug=request.get_full_path().split('/')[-2])}}
+    context = {
+        'phone': phone
+    }
     return render(request, template, context)
